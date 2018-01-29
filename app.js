627fc91f3b98db8eb969bd069e405b0d8fac2855
Fry <s_fry3@tss-hr420-dm37.fc.ca>
2018-01-29 13:34:48 -0500

    Final of week 2, Chat app started with socket, and connected and disconnect works in console

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const io = require('socket.io')(); //The () at the end incentiates it so you can use it
 
 app.use(express.static('public'));
 
@@ -11,7 +12,22 @@ app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
 
 
-
-app.listen(3000, () => {
+//Added sever = to the listen
+const server = app.listen(3000, () => {
   console.log('app running on port 3000!');
+});
+
+
+//Socket - Get it up and running
+io.attach(server);
+
+io.on('connection', (socket)=> {
+  console.log('a user has connected!');
+  //io.emit('connectMsg', {for: 'everyone', msg: `${socket.id} is here!`});
+
+  socket.on('disconnect', () => {
+    console.log('A user has been disconnectted');
+  });
+
+
 });
